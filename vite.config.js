c6b7fd8705b62ef33b0aacc19e0d0e7558022f13
auliaramdan <muhammadauliaramdan@gmail.com>
2024-04-23 15:32:57 +0700

    Dragonbones

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl"
+import {defineConfig} from "vite"
+import {resolve} from 'path'
+import {fileURLToPath} from 'node:url'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            external: [
+                fileURLToPath(
+                    new URL(
+                        'js/ar.js',
+                        import.meta.url
+                    )
+                )
+            ],
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                dragonbones: resolve(__dirname, 'dragonbones/index.html')
+            },
+        }
+    },
+    base: '/product-xr-ar/',
+    outDir: './../dist',
+    plugins: [
+        viteBasicSslPlugin()
+    ]
+})
